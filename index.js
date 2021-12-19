eeb93e17ebbe91e7ff37a5b9344838adc7c7900f
Vladimir <bobbydigital92@gmail.com>
2021-12-19 23:17:48 +0300

    use shadow DOM

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ class HelloButton extends HTMLButtonElement {
 }
 
 customElements.define('hello-button', HelloButton, {extends: 'button'});
+
+class ShowHello extends HTMLElement {
+    connectedCallback() {
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = `<p><span>
+            Hello, ${this.getAttribute('name')}
+        </span></p>`
+    }
+}
+
+customElements.define('show-hello', ShowHello);
